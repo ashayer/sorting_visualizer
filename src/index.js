af53dae95d7b50081bdc96bb9539f718c438b28f
Aran <48372721+ashayer@users.noreply.github.com>
2021-07-08 01:38:57 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+document.body.style = 'background: #444444;';
+
+ReactDom.render(<App/>, document.getElementById("root"));
